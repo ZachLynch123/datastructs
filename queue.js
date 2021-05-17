a85a4d1376f3933166536eb9262c032e8bc12243
Zach Lynch <zachary.lynch1337@gmail.com>
2021-05-17 16:33:00 -0700

    Created a queue class with methods to add, remove, and get length of queue

---
+++ queue.js
@@ -0,0 +1,37 @@
+class Queue {
+    constructor() {
+        this.elemets = [];
+    }
+    // Add to Queue
+    enqueue(e) {
+        this.elemets.push(e);
+    }
+    // Remove element from the front of queue
+    dequeue(e) {
+        return this.elemets.shift();
+    }
+    // Check if Queue is Empty
+    isEmpty() {
+        return this.elemets.length == 0;
+    }
+    // Get element at front of Queue
+    peek() {
+        return !this.isEmpty() ? this.elemets[0] : undefined; // If queue isn't empty, grab element from front of Queue
+    }
+    // Get length of Queue
+    length() {
+        return this.elemets.length;
+    }
+
+}
+
+let queue = new Queue();
+// Simple for loop to add 7 elements to queue
+for (let i = 1; i <= 7; i++) {
+    q.enqueue(i);
+}
+console.log(q.length);
+
+
+
+
