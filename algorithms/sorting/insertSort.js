d1c43dae3d97e5c8c4a567549dfcc5942f44cf01
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-10 09:17:20 -0700

    created algorithm that takes lowest number of array and adds it to new array, thus sorting the data

---
+++ algorithms/sorting/insertSort.js
@@ -0,0 +1,19 @@
+// use selection sort algorithm to sort array
+
+let sortedArray = []
+let unsortedArray = [5, 6, -1, 1, 3];
+
+
+const minAndRemove = (array) => {
+    let min = array[0];
+    let minIndex = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+    array.splice(minIndex, 1);
+    return min;
+}
